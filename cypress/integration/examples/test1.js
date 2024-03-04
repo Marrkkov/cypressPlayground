8c3a026faa846e834a74749b4cac98b8374c12b2
Marko <marko.vasquez@gmail.com>
2024-03-04 20:42:55 -0300

    firstTestImplementation

---
+++ cypress/integration/examples/test1.js
@@ -0,0 +1,12 @@
+//cypress - Spec
+describe('Random Test Suite', function() {
+    
+    it('Test1', function() {
+        cy.visit('www.google.com');
+    });
+
+    it('Test2', function() {
+        cy.visit('www.microsoft.com');
+    });
+
+});
